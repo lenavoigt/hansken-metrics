acb1987fadacdb62bec3e1d979bb3ef8068b75a7
Lena <lena.lucia.voigt@fau.de>
2025-05-15 16:27:46 +0200

    add brief explanation to config file

--- config/environment_config.example.py
+++ config/environment_config.example.py
@@ -1,6 +1,13 @@
+# This configuration file is used to load user specific configurations (which gatekeeper/keystore etc.) need to be used for connecting with Hansken
+
 gatekeeper = f''
+
 keystore = f''
+
 username = f''
+
 password = f''
+
 verify = True
+
 interactive = True
